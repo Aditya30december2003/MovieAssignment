487c1ef471f1ef52fdce19acfa0fea497bc43548
Aditya30december2003 <adityasmjain@gmail.com>
2024-07-17 22:42:43 +0530

    change link

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <>
     <Navbar />
     <Routes>
-     <Route path='/MovieAssignment/' element={<Home />} />
+     <Route path='/movieAssignment/' element={<Home />} />
     </Routes>
     <Footer />
     </>
